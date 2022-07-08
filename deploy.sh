c9af818e6ac61583763c038daf8b3aa1df2783ee
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-09 01:22:37 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,9 @@ sudo service docker start
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-
+sudo yum install -y python3
+sudo yum install -y python3-pip
+sudo pip3 install docker-compose
 #sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 #wait 10
 #sudo chmod +x /usr/local/bin/docker-compose
