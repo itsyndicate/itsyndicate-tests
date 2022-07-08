a11e13efe4109a3aa2493c89283becf12362a5ad
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-09 00:15:37 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,7 @@ sudo service docker start
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-sudo dnf install python3
-sudo python3 -m pip install -U pip
-python3 -m pip install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
 docker-compose up -d
