7927849b48e91c8b6dbf362e185bf591ce03ae49
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-09 00:19:44 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ sudo service docker start
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose version
 docker-compose up -d
