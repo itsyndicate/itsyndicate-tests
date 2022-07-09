30380ae6e05f6682748a47e2cb650f36417a3c21
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-09 11:51:52 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,10 @@ sudo yum update -y
 sudo amazon-linux-extras install -y docker
 sudo service docker start
 sudo groupadd docker
+sudo gpasswd -a username docker
 sudo usermod -aG docker $USER
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo docker-compose up -d
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo service docker restart
+docker-compose up -d
