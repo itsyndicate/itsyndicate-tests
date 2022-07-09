3b000421fc80f7a051f3f0f5fa1e298bf0dc5678
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-09 23:28:46 +0300

    deploy and nginx config

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -113,8 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = "/application/templates"
+STATIC_URL = "/static/"
+STATIC_ROOT = "/usr/share/nginx/html/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -45,7 +45,8 @@ resource "null_resource" "copy_files" {
       "bash deploy.sh",
       "docker-compose exec web python /application/manage.py collectstatic --no-input",
       "docker-compose exec web python /application/manage.py makemigrations --no-input",
-      "docker-compose exec web python /application/manage.py migrate --run-syncdb --no-input"
+      "docker-compose exec web python /application/manage.py migrate --run-syncdb --no-input",
+      "docker-compose exec web python /application/manage.py startapp upload --no-input"
     ]
   }
 
