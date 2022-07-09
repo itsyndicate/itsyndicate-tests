da6e9ed94a7deed8c6143b19910746bcf08532c4
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-09 14:23:26 +0300

    deploy

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -12,30 +12,20 @@ resource "aws_key_pair" "ssh_key_pair" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 output "ssh-key-private" {
-  value = tls_private_key.ssh_key.private_key_pem
+  value     = tls_private_key.ssh_key.private_key_pem
   sensitive = true
 }
 
 resource "aws_instance" "backend" {
   ami                         = "ami-0a1ee2fb28fe05df3"
   instance_type               = "t2.micro"
+  iam_instance_profile        = "containerise"
   subnet_id                   = aws_subnet.public-1a.id
   associate_public_ip_address = true
   security_groups             = [aws_security_group.backend.id]
   key_name                    = aws_key_pair.ssh_key_pair.key_name
   vpc_security_group_ids      = [aws_security_group.backend.id]
-  user_data                   = <<EOF
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo apt-get install -y docker-compose
-EOF
-
-  tags = {
+  tags                        = {
     Name = "django-backend"
   }
   depends_on = [aws_security_group.backend, aws_key_pair.ssh_key_pair]
