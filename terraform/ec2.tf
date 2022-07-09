9e18debf0cdf46132f8b40ecf8d2366de35b55c7
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-09 16:55:20 +0300

    deploy

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -43,7 +43,7 @@ resource "null_resource" "copy_files" {
       "git clone https://github.com/inemyrovsk/itsyndicate-tests.git",
       "cd itsyndicate-tests",
       "bash deploy.sh",
-      "docker-compose exec web python /application/manage.py collectstatic -y"
+      "docker-compose exec web python /application/manage.py collectstatic --no-input"
     ]
   }
 
