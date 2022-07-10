55c7fdf010d985d2de74a826041881a8b490b493
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-10 16:15:13 +0300

    target origin id changed

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -12,7 +12,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD",]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "http://static1index-image.s3-website.eu-central-1.amazonaws.com"
+    target_origin_id       = "static1index-image.s3.eu-central-1.amazonaws.com"
     compress               = true
     cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
     viewer_protocol_policy = "allow-all"
