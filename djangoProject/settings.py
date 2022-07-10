7dda9fcc562449b717961648a2b66142d7ebeecd
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-10 10:37:40 +0300

    deploy and nginx config

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -113,10 +113,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = "/usr/share/nginx/html/"
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# STATIC_URL = "/static/"
+# STATIC_ROOT = "/usr/share/app/staticfiles"
+# MEDIA_URL = '/usr/src/app/mediafiles'
+# MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
