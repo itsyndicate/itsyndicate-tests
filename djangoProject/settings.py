1544672b704b9903c9f5eaffec3cb884f11d0626
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-07-10 17:58:38 +0300

    hello_django module

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -130,14 +130,11 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 # s3 static settings
 STATIC_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-STATICFILES_STORAGE = 'hello_django.storage_backends.StaticStorage'
 # s3 public media settings
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'hello_django.storage_backends.PublicMediaStorage'
 # s3 private media settings
 PRIVATE_MEDIA_LOCATION = 'private'
-PRIVATE_FILE_STORAGE = 'hello_django.storage_backends.PrivateMediaStorage'
 
 
 
